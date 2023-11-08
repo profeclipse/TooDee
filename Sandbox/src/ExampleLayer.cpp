119930aeafb7342ef3a0384166c16df1bee95fea
profeclipse <davehunt@me.com>
2023-11-07 20:29:44 -0500

    General maintenance

---
+++ Sandbox/src/ExampleLayer.cpp
@@ -0,0 +1,27 @@
+#include <imgui/imgui.h>
+#include "ExampleLayer.h"
+
+ExampleLayer::ExampleLayer()
+    : Layer("Example")
+{
+}
+
+void ExampleLayer::OnUpdate(TooDee::TimeStep ts)
+{
+    //TD_INFO("ExampleLayer::Update");
+}
+
+void ExampleLayer::OnEvent(TooDee::Event& e)
+{
+    //TD_TRACE("{0}",e);
+}
+
+void ExampleLayer::OnImGuiRender()
+{
+    bool state = true;
+    ImGui::ShowDemoWindow(&state);
+
+    ImGui::Begin("Hello");
+    ImGui::Text("Hello, World!");
+    ImGui::End();
+}
--- Sandbox/src/ExampleLayer.h
+++ Sandbox/src/ExampleLayer.h
@@ -1,33 +1,13 @@
 #pragma once
 
-#include <imgui/imgui.h>
 #include <TooDee/TooDee.h>
 
 class ExampleLayer : public TooDee::Layer
 {
     public:
-        ExampleLayer()
-            : Layer("Example")
-        {
-        }
+        ExampleLayer();
 
-        virtual void OnUpdate(TooDee::TimeStep ts) override
-        {
-            //TD_INFO("ExampleLayer::Update");
-        }
-
-        virtual void OnEvent(TooDee::Event& e) override
-        {
-            //TD_TRACE("{0}",e);
-        }
-
-        virtual void OnImGuiRender() override
-        {
-            bool state = true;
-            ImGui::ShowDemoWindow(&state);
-
-            ImGui::Begin("Hello");
-            ImGui::Text("Hello, World!");
-            ImGui::End();
-        }
+        virtual void OnUpdate(TooDee::TimeStep ts) override;
+        virtual void OnEvent(TooDee::Event& e) override;
+        virtual void OnImGuiRender() override;
 };
